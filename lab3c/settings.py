139a538de95fdd580a20668cf8d2ac1719721729
Jose Saul Vega Arrieta <rsi.dev15@uis.edu.co>
2022-05-12 16:47:22 -0500

    fix: comment cloudinary

--- lab3c/settings.py
+++ lab3c/settings.py
@@ -172,9 +172,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ['CLOUD_NAME'],
-    'API_KEY': os.environ['API_KEY'],
-    'API_SECRET': os.environ['API_SECRET'],
-}
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': os.environ['CLOUD_NAME'],
+#     'API_KEY': os.environ['API_KEY'],
+#     'API_SECRET': os.environ['API_SECRET'],
+# }
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
