dba31030be48f3e0c2c0cfd9c257a8a1895732c7
Jose Saul Vega Arrieta <rsi.dev15@uis.edu.co>
2022-05-13 14:57:55 -0500

    fix: admin url

--- lab3c/urls.py
+++ lab3c/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('doctor-who/', admin.site.urls),
+    path('doctor-who', admin.site.urls),
     path('', include('base.urls')),
 ]
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
