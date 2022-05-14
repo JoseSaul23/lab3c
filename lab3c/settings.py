016ec99c168d62f966358cac1f1e7245584b954f
Jose Saul Vega Arrieta <rsi.dev15@uis.edu.co>
2022-05-14 13:30:38 -0500

    fix: use contentDisposition

--- lab3c/settings.py
+++ lab3c/settings.py
@@ -176,6 +176,9 @@ AWS_S3_OBJECT_PARAMETERS = {
     "ACL": "public-read"
 }
 AWS_S3_FILE_OVERWRITE = False
+AWS_S3_OBJECT_PARAMETERS = {
+    'Content-Disposition': 'attachments;',
+}
 
 AWS_LOCATION = "https://lab3c-space.nyc3.digitaloceanspaces.com"
 DEFAULT_FILE_STORAGE = 'lab3c.storage_backends.MediaRootS3BotoStorage'
