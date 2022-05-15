ec7cf8f7ce2c33b0550ccbf0d16b62e01861e83b
Jose Saul Vega Arrieta <rsi.dev15@uis.edu.co>
2022-05-14 19:24:04 -0500

    fix: no more download button

--- lab3c/settings.py
+++ lab3c/settings.py
@@ -172,7 +172,6 @@ AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
-    'ContentDisposition': 'attachments;',
     "CacheControl": "max-age=86400",
     "ACL": "public-read"
 }
